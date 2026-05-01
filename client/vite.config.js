0c2358b31b9dcd860d4fc5821bb63535d04b68cd
Aadii-stack <aaditya.r.mirashi25@slrtce.in>
2026-05-01 23:24:25 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
       '/api': 'http://127.0.0.1:5001',
       '/uploads': 'http://127.0.0.1:5001'
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'leaflet': ['leaflet'],
+          'jspdf': ['jspdf'],
+          'supabase': ['@supabase/supabase-js']
+        }
+      }
+    }
   }
 });
